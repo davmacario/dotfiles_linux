4183e59648b026bf780e3168ceb1edd197ec4b60
davmacario <dav,macario@gmail.com>
2023-12-31 16:48:54 +0100

    Fix treesitter markdown setting s

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -13,7 +13,7 @@ require'nvim-treesitter.configs'.setup {
       "javascript",
       "python",
       "yaml",
-      --"markdown",
+      "markdown",
       "matlab",
       "comment",
       "css",
@@ -30,7 +30,7 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (or "all")
-  ignore_install = { "markdown" },
+  -- ignore_install = { "markdown" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
